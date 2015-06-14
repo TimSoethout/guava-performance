02ee94613face8ccb15d7596b8d54399b0ff0e91
Tim Soethout <tim@glukorizon.nl>
2015-06-14 21:48:31 +0200

    Scalameter reports, also now using strings and containing nulls

--- src/test/scala/nl/timmybankers/guava/FilterNullsBenchMark.scala
+++ src/test/scala/nl/timmybankers/guava/FilterNullsBenchMark.scala
@@ -3,24 +3,38 @@ package nl.timmybankers.guava
 import java.util
 
 import org.scalameter.api._
+
 import scala.collection.JavaConverters._
 
 object FilterNullsBenchMark extends PerformanceTest.OfflineReport {
-  val sizes: Gen[Int] = Gen.range("size")(0, 1500000, 300000)
+  val sizes: Gen[Int] = Gen.exponential("size")(1, 1500000, 2)
+  //  val nullIndices = Gen.range("nullIndices")(0, 150000, 10)
 
-  val ranges: Gen[util.List[Int]] = for {
-    size <- sizes
-  } yield (0 until size).toList.asJava
+  val ranges: Gen[util.List[String]] =
+    sizes.map(size => (0 until size)
+      .zipWithIndex.map { case (i: Int, v: Int) => if (i % 9 == 0) null else v.toString }
+      .toList.asJava)
 
-//  override lazy val reporter = ChartReporter(ChartFactory.XYLine())
+  //  override lazy val reporter = ChartReporter(ChartFactory.XYLine())
 
   performance of "Range" in {
     measure method "filterNullsPlain" in {
       using(ranges) in {
-        input => FilterNulls.filterNullsPlain(input).asScala foreach (_ => Unit)
+        input => FilterNulls.filterNullsPlain(input) //.asScala foreach (_ => Unit)
       }
     }
     measure method "filterNullsGuava" in {
+      using(ranges) in {
+        input => FilterNulls.filterNullsGuava(input) //.asScala foreach (_ => Unit)
+      }
+    }
+
+    measure method "filterNullsPlainStrictEvaluation" in {
+      using(ranges) in {
+        input => FilterNulls.filterNullsPlain(input).asScala foreach (_ => Unit)
+      }
+    }
+    measure method "filterNullsGuavaStrictEvaluation" in {
       using(ranges) in {
         input => FilterNulls.filterNullsGuava(input).asScala foreach (_ => Unit)
       }
