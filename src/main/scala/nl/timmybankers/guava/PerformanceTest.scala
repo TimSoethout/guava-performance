bbdfec5d587a28a0af27f43ded6cd3e97277c903
Tim Soethout <tim@glukorizon.nl>
2015-06-14 17:28:56 +0200

    Added Map performance test, refactored compare

--- src/main/scala/nl/timmybankers/guava/PerformanceTest.scala
+++ src/main/scala/nl/timmybankers/guava/PerformanceTest.scala
@@ -1,49 +1,68 @@
 package nl.timmybankers.guava
 
-import java.util
 import java.util.concurrent.TimeUnit
 
 import com.google.common.base.Stopwatch
+import org.scalacheck.Gen
 
 import scala.concurrent.duration._
 
 object PerformanceTest extends App {
-  // warm up JVM
-  0 to 1000 foreach {
-    _ =>
-      val input: util.Collection[String] = Helpers.listWithNullGenerator.sample.get
-      FilterNulls.filterNullsGuava(input)
-      FilterNulls.filterNullsPlain(input)
-  }
 
-  val inputs: Seq[util.Collection[String]] =
-    0 to 1000 map {
-      _ => Helpers.listWithNullGenerator.sample.get
+
+  // First compare Null filter on list
+  compare(Helpers.listWithNullGenerator, "Guava", {
+    FilterNulls.filterNullsGuava[String]
+  }, "Plain", {
+    FilterNulls.filterNullsPlain[String]
+  })
+
+  // Now the real use case on Map
+  compare(Helpers.mapWithNullValuesGenerator, "Guava", {
+    FilterNulls.filterBlanksInMapGuava[String]
+  }, "Plain", {
+    FilterNulls.filterBlanksInMapPlain[String]
+  })
+
+  def compare[T](generator: Gen[T], firstLabel: String, first: T => T, secondLabel: String, second: T => T) = {
+    // warm up JVM
+    0 to 1000 foreach {
+      _ =>
+        val input: T = generator.sample.get
+        first(input)
+        second(input)
     }
 
-  val (javaWins, guavaWins): (Int, Int) = inputs.foldLeft((0, 0)) {
-    case ((prevJavaWins, prevGuavaWins), input) =>
-      println(s"run for input with ${input.size}")
-
-      val guavaTime = time {
-        FilterNulls.filterNullsGuava(input)
+    val inputs: Seq[T] =
+      0 to 1000 map {
+        _ => generator.sample.get
       }
-      println(s"Guava took: \t\t $guavaTime")
 
-      val javaTime = time {
-        FilterNulls.filterNullsPlain(input)
-      }
-      println(s"Plain  took: $javaTime")
+    val (firstWins, secondWins): (Int, Int) = inputs.foldLeft((0, 0)) {
+      case ((prevFirstWins, prevSecondWins), input) =>
+        //        println(s"run for input with ${input.size}")
 
-      println(s"Difference (java-guava): ${javaTime - guavaTime}")
-      if (javaTime > guavaTime) {
-        (prevJavaWins, prevGuavaWins + 1)
-      } else {
-        (prevJavaWins + 1, prevGuavaWins)
-      }
+        val firstTime = time {
+          first(input)
+        }
+        println(s"$firstLabel took: \t\t $firstTime")
+
+        val secondTime = time {
+          second(input)
+        }
+        println(s"$secondLabel  took: $secondTime")
+
+        println(s"Difference (second-first): ${secondTime - firstTime}")
+        if (secondTime < firstTime) {
+          (prevFirstWins, prevSecondWins + 1)
+        } else {
+          (prevFirstWins + 1, prevSecondWins)
+        }
+
+    }
+    println(s"$firstLabel fastest #$firstWins; $secondLabel fastest #$secondWins")
   }
-  
-  println(s"Plain fastest #$javaWins; Guava fastest #$guavaWins")
+
 
   def time(function: => Unit): Duration = {
     val timer = Stopwatch.createUnstarted()
