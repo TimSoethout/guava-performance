7201ed6105d7c5150283a0985b6bfeb987811092
Tim Soethout <tim@glukorizon.nl>
2015-06-14 22:00:24 +0200

    Added Map benchmarks

--- src/test/scala/nl/timmybankers/guava/FilterNullsBenchMark.scala
+++ src/test/scala/nl/timmybankers/guava/FilterNullsBenchMark.scala
@@ -10,33 +10,49 @@ object FilterNullsBenchMark extends PerformanceTest.OfflineReport {
   val sizes: Gen[Int] = Gen.exponential("size")(1, 1500000, 2)
   //  val nullIndices = Gen.range("nullIndices")(0, 150000, 10)
 
-  val ranges: Gen[util.List[String]] =
+  val lists: Gen[util.List[String]] =
     sizes.map(size => (0 until size)
       .zipWithIndex.map { case (i: Int, v: Int) => if (i % 9 == 0) null else v.toString }
       .toList.asJava)
 
+  val maps: Gen[util.Map[String,String]] =
+    sizes.map(size => (0 until size)
+      .map(i => i.toString -> (if (i % 9 == 0) null else i.toString))
+      .toMap.asJava)
+  
   //  override lazy val reporter = ChartReporter(ChartFactory.XYLine())
 
-  performance of "Range" in {
-    measure method "filterNullsPlain" in {
-      using(ranges) in {
-        input => FilterNulls.filterNullsPlain(input) //.asScala foreach (_ => Unit)
-      }
-    }
-    measure method "filterNullsGuava" in {
-      using(ranges) in {
-        input => FilterNulls.filterNullsGuava(input) //.asScala foreach (_ => Unit)
-      }
-    }
+  performance of "FilterNulls" in {
+//    measure method "filterNullsPlain" in {
+//      using(ranges) in {
+//        input => FilterNulls.filterNullsPlain(input) //.asScala foreach (_ => Unit)
+//      }
+//    }
+//    measure method "filterNullsGuava" in {
+//      using(ranges) in {
+//        input => FilterNulls.filterNullsGuava(input) //.asScala foreach (_ => Unit)
+//      }
+//    }
 
     measure method "filterNullsPlainStrictEvaluation" in {
-      using(ranges) in {
+      using(lists) in {
         input => FilterNulls.filterNullsPlain(input).asScala foreach (_ => Unit)
       }
     }
     measure method "filterNullsGuavaStrictEvaluation" in {
-      using(ranges) in {
+      using(lists) in {
         input => FilterNulls.filterNullsGuava(input).asScala foreach (_ => Unit)
+      }
+    }
+
+    measure method "filterBlanksInMapPlain" in {
+      using(maps) in {
+        input => FilterNulls.filterBlanksInMapPlain(input).asScala foreach (_ => Unit)
+      }
+    }
+    measure method "filterBlanksInMapGuava" in {
+      using(maps) in {
+        input => FilterNulls.filterBlanksInMapGuava(input).asScala foreach (_ => Unit)
       }
     }
   }
